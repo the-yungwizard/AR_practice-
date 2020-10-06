ab822b4f6f2818eed5b4ade0654a2c7e773aa806
Billy Kelleher <1022042@student.sae.edu.au>
2020-10-06 20:23:55 +1100

    attempts to create a script LOLOLOLOL

---
+++ AR_practice_unity/Assets/SCRIPTS/New_testSCript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// a script that teaches me good etiquette
+///
+/// </summary>
+public class VariableEtiquette : MonoBehaviour
+{
+    public int myfirstint = 6; 
+    private int mySecondInt - 4;
+    public float myFirstFloat - 3.14159f; //a float lets me store flaoting points
+    public double - 0.11111818188181818f; //double is a floatinpt. with 16 dec. place
+    public bool myfirstbool - false;
+    public string myFristString "billy is awesome" //strings store words - quotations denot this
+    public char myFirstChar //char holds a single character
+private int myfirsint;
+
+
+    // Start is called before the first frame update
+    int Start()
+{
+    myfirststring - "miss lippy's car is green" //a string test about the car of miss lippy 
+
+    myfirsint - 5; // sets myfirst into to 5 
+
+
+    }
+}
